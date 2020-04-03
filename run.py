e0674d4a71c9c3e541c0fe1119d449c64692b533
ZhengYi <nuke504@gmail.com>
2020-04-03 14:59:35 +0800

    added cosine annealing

--- run.py
+++ run.py
@@ -1,82 +1,25 @@
-from utils import *
-from model import *
+from utils import iteratefromdict, log_print, get_data
+from model import StarTrekLSTM, train_model
+
+import os
 import torch
 import logging
-
-# def run(epochs, rnn, train_dataloader, validation_dataloader, criterion, device, optimizer, scheduler, print_every, sample_every):
-#     start = time.time()
-#     all_losses = []
-#     all_acc = []
-#     all_val_losses = []
-#     all_val_acc = []
-    
-#     for epoch in range(1, epochs + 1):
-#         print(f"Epoch: {epoch}")
-#         count = 0
-#         acc_per_epoch = []
-#         loss_per_epoch = []
-
-#         # Train on training set
-#         train_start = time.time()
-#         rnn.train()
-#         print(f"Training ({epoch}/{epochs})")
-#         for idx, batch_category_lines in enumerate(train_dataloader):
-#             count += len(batch_category_lines)
-#             input_tensor_batch = []
-#             target_tensor_batch = []
-#             for line in batch_category_lines:
-#                 input_tensor_batch.append(inputTensor(line).squeeze(1))
-#                 target_tensor_batch.append(targetTensor(line))
-
-#             input_tensor_batch_padded = pad_packed_sequence(pack_sequence(input_tensor_batch, enforce_sorted=False))[0]
-#             target_tensor_batch_padded = pad_packed_sequence(pack_sequence(target_tensor_batch, enforce_sorted=False))[0]
-
-#             loss, acc = train(input_tensor_batch_padded, target_tensor_batch_padded, criterion, device, optimizer, scheduler, rnn)
-#             acc_per_epoch.append(acc)
-#             loss_per_epoch.append(loss)
-#             if count % print_every == 0:
-#                 print('%s (%d %d%%) %.4f' % (timeSince(start), count, count / train_dataloader.num() * 100, np.sum(loss_per_epoch)/count))
-#                 # print(f'Training Acc: {np.sum(acc_per_epoch)/(count}')
-#             if count % sample_every == 0:
-#                 print(sample(rnn))
-#                 logging.info(f'Trainined on {count} examples:')
-#                 for i in range(1,11):
-#                     logging.info(f'{i}. {sample(rnn)}')
-#         print(f'Training Time taken: {timeSince(train_start)}')
-        
-#         val_start = time.time()
-#         # Evaluate on validation set
-#         print(f"Evaluating ({epoch}/{epochs})")
-#         val_acc_per_epoch, val_loss_per_epoch = evaluate(rnn, validation_dataloader, device, criterion)
-#         print(f'Validation Time taken: {timeSince(val_start)}')
-#         print(f'Validation Accuracy: {val_acc_per_epoch}')
-#         print(f'Validation Loss: {val_loss_per_epoch}')
-
-#         logging.info(f'Epoch: {epoch}')
-#         logging.info(f'Training Acc: {np.mean(acc_per_epoch)}')
-#         logging.info(f'Training Loss: {np.mean(loss_per_epoch)}')
-#         logging.info(f'Validation Acc: {val_acc_per_epoch}')
-#         logging.info(f'Validation Loss: {val_loss_per_epoch}')
-
-#         all_acc.append(np.mean(acc_per_epoch))
-#         all_losses.append(np.mean(loss_per_epoch))
-#         all_val_losses.append(val_loss_per_epoch)
-#         all_val_acc.append(val_acc_per_epoch)
-
-#     return all_losses, all_acc, all_val_losses, all_val_acc
+import string
+import time
 
 if __name__ == "__main__":
     # Build the category_lines dictionary, a list of lines per category
     category_lines,all_categories = get_data()
     starting_time = time.time()
     category_lines = category_lines['st']
+    category_lines = [line for line in category_lines if len(line) < 400]
     
     training = True
     plotting = True 
 
     train_percent = 0.8
     valid_percent = 0.1
-    batch_size = 16
+    batch_size = 6
 
     train_dataloader = iteratefromdict(category_lines, train = True, seed = 5, batch_size = batch_size)
     val_dataloader = iteratefromdict(category_lines, train = False, seed = 5, batch_size = batch_size)
@@ -99,31 +42,13 @@ if __name__ == "__main__":
     log_print(f'No. of layers: {n_layers}', logging)
     log_print(f'Hidden size: {hidden_size}', logging)
 
-    stLSTM = StarTrekLSTM(input_size = n_letters,
-                        hidden_size = hidden_size,
-                        output_size = n_letters,
-                        n_layers = n_layers)
-    # epochs = 1
-    # print_every = 1000
-    # plot_every = 1000
-    # sample_every = 1000
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    stLSTM = StarTrekLSTM(input_size = n_letters, hidden_size = hidden_size, output_size = n_letters, n_layers = n_layers)
+    # stLSTM = torch.load(os.path.join(os.getcwd(),'model','best_model.pth'))
+
     criterion = torch.nn.CrossEntropyLoss(ignore_index=-1) # Ignore the padding index -1
     learning_rate = 0.001
     optimizer = torch.optim.Adam(stLSTM.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [1,2,3,4,5,6,7,8,9], gamma=0.5)
-
-    # # Run on training and validation set
-    # if training:
-    #     all_losses, all_acc, all_val_losses, all_val_acc = run(epochs, rnn, train_dataloader, val_dataloader, criterion, device, optimizer, scheduler, print_every, sample_every)
-
-    # # Plot results
-    # if plotting:
-    #     plot(all_losses, all_val_losses, all_acc, all_val_acc)
-
-    # logging.info(f'Total time taken: {timeSince(starting_time)}')
-
-    # Define Loss and Accuracy Metric
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 10, T_mult=1, eta_min=0)
     loss = torch.nn.CrossEntropyLoss(ignore_index=-1) # Ignore the padded class
 
     train_model(train_dataloader = train_dataloader,
@@ -133,7 +58,7 @@ if __name__ == "__main__":
                 optimizer = optimizer,
                 scheduler = scheduler,
                 batch_size = batch_size,
-                num_epochs = 10,
+                num_epochs = 20,
                 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
                 logger = logging,
                 verbose = True,
--- utils.py
+++ utils.py
@@ -215,7 +215,7 @@ def get_data():
     all_categories = ['st']
     category_lines['st']=[]
     filterwords=['NEXTEPISODE']
-    with open('./star_trek_transcripts_all_episodes_f.csv', newline='') as csvfile:
+    with open('./star_trek_transcripts_all_episodes.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         for row in reader:
             for el in row:
@@ -237,35 +237,6 @@ def targetTensor(line):
     letter_indexes = [all_letters.find(line[li]) for li in range(1, len(line))]
     letter_indexes.append(n_letters - 1) # EOS
     return torch.LongTensor(letter_indexes)
-
-# def timeSince(since):
-#     now = time.time()
-#     s = now - since
-#     m = math.floor(s / 60)
-#     s -= m * 60
-#     return '%dm %ds' % (m, s)
-
-# Sample from a category and starting letter
-# def sample(rnn, max_length = 200):
-#     rnn.cpu()
-#     caps_letters = 'ABCDEFGHIJKLMNOPRSTUVWZ'
-#     start_letter = caps_letters[np.random.choice(len(caps_letters))]
-#     with torch.no_grad():  # no need to track history in sampling
-#         input = inputTensor(start_letter)
-#         hidden = rnn.init_Hidden_n_Cell_sample()
-
-#         output_name = start_letter
-
-#         for _ in range(max_length):
-#             output, hidden = rnn(input, hidden, sample  = True)
-#             topi = np.random.choice(a = output.shape[1], size = output.shape[0], p = output.squeeze().numpy())[0]
-#             if topi == n_letters - 1:
-#                 break
-#             else:
-#                 letter = all_letters[topi]
-#                 output_name += letter
-#             input = inputTensor(letter)
-#         return output_name
 
 def plot(train_losses, val_losses, train_acc, val_acc):
     fig, ax = plt.subplots(1,2, figsize = (10,5))
